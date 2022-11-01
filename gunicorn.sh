a16d4641e97eb96d17f4266eb335a974702419cd
shalva kakhurashvili <kakhurashvilis@gmail.com>
2022-11-01 23:18:12 +0400

    jenkins-3

--- gunicorn.sh
+++ gunicorn.sh
@@ -10,7 +10,7 @@ python3 manage.py collectstatic -- no-input
 
 echo "Migrations done"
 
-cd /var/lib/jenkins/workspace/Django_system
+cd /var/lib/jenkins/workspace/project/Django_system
 
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
