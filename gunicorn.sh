2e6a60f8c9320499d9235374f5a3d42e3c45c56f
shalva kakhurashvili <kakhurashvilis@gmail.com>
2022-11-01 23:26:21 +0400

    jenkins-4

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 
 source env/bin/activate
 
-cd /var/lib/jenkins/workspace/project/Django_system
+cd /var/lib/jenkins/workspace/project/
 
 python3 manage.py makemigrations
 python3 manage.py migrate
