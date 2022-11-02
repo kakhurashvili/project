04788fe962ef756c27d3b82692a18201c75c5223
shalva kakhurashvili <kakhurashvilis@gmail.com>
2022-11-02 22:05:11 +0400

    jenkins-25

--- nginx.sh
+++ nginx.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/sites-available/app
+#!/bin/sh
+
+sudo cp -rf app.conf /etc/nginx/conf.d
+
 chmod 777 /var/lib/jenkins/workspace/django_project
-
-
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
 sudo nginx -t
 
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo systemctl reload nginx
 
-echo "Nginx has been started"
+sudo systemctl restart nginx
 
 sudo systemctl status nginx
