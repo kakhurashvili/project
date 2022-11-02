f6165d0654a506a3c4285cda9674e0d388c9a2a7
shalva kakhurashvili <kakhurashvilis@gmail.com>
2022-11-02 15:28:54 +0400

    jenkins-27

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf app.conf /etc/nginx/sites-available/app
 chmod 777 /var/lib/jenkins/workspace/django_project
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/app.conf /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
