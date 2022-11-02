2634c4ff381a9d02f91c29436680524ccf0c65bd
shalva kakhurashvili <kakhurashvilis@gmail.com>
2022-11-02 14:58:10 +0400

    jenkins-19

--- nginx.sh
+++ nginx.sh
@@ -3,7 +3,7 @@
 sudo cp -rf medashan.conf /etc/nginx/sites-available/medashan
 chmod 777 /var/lib/jenkins/workspace/django_project
 
-sudo ln -s /etc/nginx/sites-available/medashan /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/medashan.conf /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
